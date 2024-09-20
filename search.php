21ca403115ffc76bfe80c9fc6f94989786bab917
Anshul-Sharma12 <anshulsharma052005@gmail.com>
2024-09-20 16:14:36 +0530

    ajax call

--- search.php
+++ search.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
 $sql = "SELECT post_name FROM wp_posts where post_name like '%$name%'";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result->num_rows > 0 && $name != NULL) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo "name : " . $row["post_name"] . "<br>" ;
